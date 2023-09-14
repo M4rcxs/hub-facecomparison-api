5a7a90ba09e5bcc3629701de0d16b1a77a121410
Marcos Silva <marcos.201314@hotmail.com>
2023-09-13 21:26:25 -0300

    updated facecomparison

--- src/test/java/com/br/hub/facecomparison/FacecomparisonApplicationTests.java
+++ src/test/java/com/br/hub/facecomparison/FacecomparisonApplicationTests.java
@@ -1,13 +1,21 @@
 package com.br.hub.facecomparison;
 
-import jakarta.ws.rs.core.Application;
+import com.br.hub.facecomparison.config.SpringApp;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = FacecomparisonApplicationTests.class)
+@SpringBootTest(classes = SpringApp.class)
 @ActiveProfiles(value = "test")
-class FacecomparisonApplicationTests {
+class SpringAppTest {
+
+	@BeforeAll
+	static void setupProperties() {
+		System.setProperty("aws.region", "us-east-1");
+		System.setProperty("aws.accessKeyId", "localstack");
+		System.setProperty("aws.secretAccessKey", "localstack");
+	}
 
 	@Test
 	void contextLoads() {
